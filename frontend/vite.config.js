4c2639db15891a6c7496e364dee2d726e476282b
malith damsara <malithdamsara87@gmail.com>
2025-07-02 12:00:09 +0530

    abc

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,10 +9,13 @@ export default defineConfig({
   plugins: [react()],
   base: './', // Changed from '/' to './'
   build: {
+    minify: 'terser',
+  sourcemap: true,
     outDir: path.resolve(__dirname, 'dist'),
     emptyOutDir: true,
     rollupOptions: {
       input: path.resolve(__dirname, 'index.html')
+
     }
   },
   server: {
